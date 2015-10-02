32d99c80b7d07511f01ded55b36f87fedde75fe8
Jonathan <jkoomjian@gmail.com>
2015-10-01 23:49:04 -0700

    removing unnecessary second gmail username/pass

--- merr_email.rb
+++ merr_email.rb
@@ -2,24 +2,22 @@ $: << File.join(File.dirname(__FILE__), '.')
 require 'smtp_tls'
 require 'mail'
 
-# same command as merr.rb, except this one sends an 
+# Same command as merr.rb, except this one sends an 
 # email to the user after finishing. Useful for a cron
-# job where you want to get a report every month
+# job where you want to receive a report each month
 
 # URL where this code lives on the web server
-# (must be publically accessible)
+# (must be publicly accessible)
 $merr_url = "http://www.jonathankoomjian.com/projects/MyEmailResponseRate/"
 $merr_dir = File.join(File.dirname(__FILE__), '.')
 
-if ARGV.length < 4
-  puts "Usage: ruby merr_email.rb gmail_username gmail_password gmail_sending_username gmail_sending_password"
+if ARGV.length < 2
+  puts "Usage: ruby merr_email.rb gmail_username gmail_password"
   exit
 end
 
 $SOURCE_USER = ARGV[0]
 $SOURCE_PASS = ARGV[1]
-$GMAIL_SENDING_USERNAME = ARGV[2]
-$GMAIL_SENDING_PASSWORD = ARGV[3]
 
 #------------- Generate Data -----------------#
 puts `ruby #{$merr_dir}/email2db.rb "#{$SOURCE_USER}" #{$SOURCE_PASS}`
@@ -39,8 +37,8 @@ class NotificationMailer
     options = {
             :address              => "smtp.gmail.com",
             :port                 => 587,
-            :user_name            => $GMAIL_SENDING_USERNAME,
-            :password             => $GMAIL_SENDING_PASSWORD,
+            :user_name            => $SOURCE_USER,
+            :password             => $SOURCE_PASS,
             :authentication       => 'plain',
             :enable_starttls_auto => true  }
     Mail.defaults do
@@ -50,7 +48,7 @@ class NotificationMailer
 
   def notification_message()
     mail = Mail.new do
-      from $GMAIL_SENDING_USERNAME
+      from $SOURCE_USER
       to $SOURCE_USER
       subject "Your email report is ready"
 
